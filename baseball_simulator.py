123a1d38173763347e500b86af5d279c62f28545
Jacob O'Donnell <jacobodonnell@gmail.com>
2010-03-07 12:41:58 -0500

    finished men on base and added balls

--- baseball_simulator.py
+++ baseball_simulator.py
@@ -1,15 +1,42 @@
 
+FIRST_BASE = 1
+SECOND_BASE = 2
+THIRD_BASE = 3
+
 class BaseballSimulator(object):
     def __init__(self):
-        self.men_on_base = []
+        self._men_on_base = []
+        self._num_balls = 0
 
-    def put_man_on_first(self):
-        if 1 not in self.men_on_base:
-            self.men_on_base.append(1)
+    def _set_man_on(self, base):
+        if base not in self._men_on_base:
+            self._men_on_base.append(base)
 
-    def is_man_on_first(self):
-        if 1 in self.men_on_base:
+    def _is_man_on_base(self, base):
+        if base in self._men_on_base:
             return True
-
         return False
 
+    def set_man_on_first(self):
+        self._set_man_on(FIRST_BASE)
+
+    def set_man_on_second(self):
+        self._set_man_on(SECOND_BASE)
+
+    def set_man_on_third(self):
+        self._set_man_on(THIRD_BASE)
+
+    def is_man_on_first(self):
+        return self._is_man_on_base(FIRST_BASE)
+
+    def is_man_on_second(self):
+        return self._is_man_on_base(SECOND_BASE)
+
+    def is_man_on_third(self):
+        return self._is_man_on_base(THIRD_BASE)
+
+    def set_num_balls(self, num_balls):
+        self._num_balls = num_balls
+
+    def get_num_balls(self):
+        return self._num_balls
--- baseball_simulator_tests.py
+++ baseball_simulator_tests.py
@@ -7,9 +7,20 @@ class TestBaseBallSimulator(unittest.TestCase):
 
     def test_men_on_base(self):
         self.assertFalse(self.baseball_simulator.is_man_on_first())
-        self.baseball_simulator.put_man_on_first()
+        self.baseball_simulator.set_man_on_first()
         self.assertTrue(self.baseball_simulator.is_man_on_first())
 
+        self.assertFalse(self.baseball_simulator.is_man_on_second())
+        self.baseball_simulator.set_man_on_second()
+        self.assertTrue(self.baseball_simulator.is_man_on_second())
+
+        self.assertFalse(self.baseball_simulator.is_man_on_third())
+        self.baseball_simulator.set_man_on_third()
+        self.assertTrue(self.baseball_simulator.is_man_on_third())
+
+    def test_balls(self):
+        self.baseball_simulator.set_num_balls(3)
+        self.assertEqual(self.baseball_simulator.get_num_balls(), 3)
 
 
 if __name__ == '__main__':
